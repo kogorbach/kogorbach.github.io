07df6d746019a22d44ece43be7ffa1bbb3b37382
kogorbach <40003296+kogorbach@users.noreply.github.com>
2019-07-16 21:45:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function PlaySound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.volume = 0.2;
-    thissound.play();
-}
-
-function StopSound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.pause();
-    thissound.currentTime = 0;
-}
-
 $("#btn").click(function()) {
-        window.open("https://kogorbach.github.io/hoodies/hoodie.html")
+        window.open("hoodies/hoodie.html")
 }
