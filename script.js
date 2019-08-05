8f44ad6b29d88f2a1b360f3921bc3b0c91b38178
kogorbach <40003296+kogorbach@users.noreply.github.com>
2019-08-05 17:08:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('#code').keydown(function (event) {
     if(event.which==13){
         event.preventDefault();//prevents form submission
-        location.href = window.document.codeform.code.value + '.html';
+        location.href = "hoodies/hoodie.hmtl";
     }
 });
