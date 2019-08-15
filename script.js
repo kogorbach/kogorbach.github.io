99bea9fd3464a407c82d58b05ba947bb006e39e0
kogorbach <40003296+kogorbach@users.noreply.github.com>
2019-08-15 14:06:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
         if (event.which === 13) {
         	let userInput = document.getElementById("code").value;
-            if(userInput == "rainbowHoodie") {
+            if(userInput == "rainbowHoodie1488") {
             	event.preventDefault(); //prevents form submission
             	location.replace("hoodiesSE/hoodie.html");
             }
