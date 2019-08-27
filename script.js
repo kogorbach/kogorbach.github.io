de96fc43905c159267fe96993576f6941446e4b8
kogorbach <40003296+kogorbach@users.noreply.github.com>
2019-08-27 17:14:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ $(document).ready(function () {
 
         if (event.which === 13) {
         	let userInput = document.getElementById("code").value;
-            event.preventDefault();
             if(userInput == "rainbowHoodie1488") {
+                event.preventDefault();
             	location.replace("hoodiesSE/hoodie.html");
             } else if(userInput == "nighty night") {
+                event.preventDefault();
             	location.replace("stories.html");
             } else if(userInput == "fluent quotes") {
+                event.preventDefault();
                 location.replace("quotes.txt");
         }
     });
