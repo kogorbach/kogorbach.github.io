341e972a43191f116f27f957d24dc47a661bd842
Konstantin <kogorbach@gmail.com>
2020-05-02 15:36:57 +0300

    removed hoodiesSE folder, minor refactoring, added return to main page button to various pages

--- script.js
+++ script.js
@@ -2,18 +2,15 @@ $(document).ready(function () {
     $('#code').keydown(function (event) {
 
         if (event.which === 13) {
-        	let userInput = document.getElementById("code").value;
-            if(userInput == "rainbowHoodie1488") {
+            let userInput = document.getElementById("code").value;
+            if (userInput === "nighty night") {
                 event.preventDefault();
-            	location.replace("hoodiesSE/hoodie.html");
-            } else if(userInput == "nighty night") {
+                location.replace("stories/stories.html");
+            } else if (userInput === "fluent quotes") {
                 event.preventDefault();
-            	location.replace("stories.html");
-            } else if(userInput == "fluent quotes") {
-                event.preventDefault();
-                location.replace("quotes/quotes.txt");
+                location.replace("quotes/quotes.html");
+            }
         }
-        }   
     });
 });
 
