5fccc1a163c82fb667c5ef73a5afaba96267a96e
kogorbach <kogorbach@gmail.com>
2022-11-28 19:08:50 +0200

    added new combo

--- dota/combos/combos.js
+++ dota/combos/combos.js
@@ -110,6 +110,14 @@ const heroes = [
     {
         id: "disruptor",
         url: "https://static.wikia.nocookie.net/dota2_gamepedia/images/9/97/Disruptor_icon.png"
+    },
+    {
+        id: "weaver",
+        url: "https://static.wikia.nocookie.net/dota2_gamepedia/images/0/09/Weaver_icon.png"
+    },
+    {
+        id: "lich",
+        url: "https://static.wikia.nocookie.net/dota2_gamepedia/images/b/bb/Lich_icon.png"
     }
 ]
 
